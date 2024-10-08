0e6c30bd423e450ac73a3200c913a6d4fefddf3a
Your Name <brandonks337@gmail.com>
2024-10-08 14:50:46 -0500

    pushing current changes.

--- README.md
+++ README.md
@@ -3,10 +3,20 @@
 ##### This application is a practical practice project focusing on developing a movie display web portal using IMDB resources.
 
 The tech stack is built using the following
-`` FE: React, Bootstrap ``
+`` FE: React, Bootstrap, Axios, FontAwesome, React-player, React-Router, MUI ``
 `` BE: Mongodb, Java, Spring ``
 
 To clarify the Mongodb is actually tied to Mongo Atlas and if you would like to utilize this project for yourself you will need to setup an account prior to getting rolling.
 
 ##### Getting Started
-``` git clone 
+```$ git clone```
+
+```$ cd movies-fe```
+
+```$ npm i```
+
+```$ npm start ```
+
+after this is done you should see the URL localhost:3000 in your terminal letting you know that the project is up and running. Copy and paste that into your webBrowser and boom there you go!
+
+For the Backend Project details please refer to the movies-api repository at https://github.com/BrandonKS337/movies-api and follow the README.md instructions on the setup/run process.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+'use cors'
+import "./App.css";
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Movies Array variable
+  const [movies, setMovies] = useState();
+
+  // Function handling GET request
+  const getMovies = async () => {
+    try {
+
+      const response = await api.get("/api/v1/movies"); // Promise statement that awaits for a response from endpoint before proceeding.
+      // TODO:  include logic that checks against HTTP response and only if response is good does it update the movies variable
+
+      console.log(response.data) //NOTE: logs data in the console window.
+
+      setMovies(response.data); //NOTE: Updates the state of the movies variable based on the returned data
+
+    } catch (err) 
+    {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getMovies()
+  },[])
+
+  return <div className="App"></div>;
 }
 
 export default App;
