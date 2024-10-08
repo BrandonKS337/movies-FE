311a64dae64c3f7f62deb5166a673d42297c6619
Your Name <brandonks337@gmail.com>
2024-10-08 16:15:46 -0500

    Fixed the issues regarding CORS erros that the tutorial skips over. The fix was to setup logic in BE that allowsOrigin requests from the baseURL defined in axiosConfig.js. I also had to change the baseURL from the odd one tutorial uses to simply localhost:8080 to target the port the backend is launched on.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-'use cors'
+"use cors";
 import "./App.css";
 import api from "./api/axiosConfig";
 import { useState, useEffect } from "react";
@@ -10,25 +10,21 @@ function App() {
   // Function handling GET request
   const getMovies = async () => {
     try {
-
       const response = await api.get("/api/v1/movies"); // Promise statement that awaits for a response from endpoint before proceeding.
       // TODO:  include logic that checks against HTTP response and only if response is good does it update the movies variable
 
-      console.log(response.data) //NOTE: logs data in the console window.
+      console.log(response.data); //NOTE: logs data in the console window.
 
       setMovies(response.data); //NOTE: Updates the state of the movies variable based on the returned data
-
-    } catch (err) 
-    {
+    } catch (err) {
       console.log(err);
     }
   };
 
-
-  //NOTE: used to refresh the list if movies is updated
+  //NOTE: used to relaunch the getMovies function if the movies array is adjusted is updated
   useEffect(() => {
-    getMovies()
-  },[])
+    getMovies();
+  }, []);
 
   return <div className="App"></div>;
 }
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io', // NOTE: Provides base address for API endpoints that client app calls
+    baseURL: 'http://localhost:8080', // NOTE: Provides base address for API endpoints that client app calls
     headers: { "ngrok-skip-browser-warning": "true"} // NOTE: Needed because during dev phase the tech used to call the endpoints is called ngrok and client http will be blocked without using this to bypass cors errors.
 });
