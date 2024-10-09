290e11a4e824a8c4c7c6a9267399c3ce39411610
Your Name <brandonks337@gmail.com>
2024-10-09 10:26:15 -0500

    A couple additional changes to the carousel but otherwise completed. Moving on to the header component.

---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons"
+import { Button } from "bootstrap"
+import { Container } from "react-bootstrap"
+import { Nav } from "react-bootstrap"
+import { Navbar } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+
+
+const Header = () => {
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Header
